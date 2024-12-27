57a1e4c37e24fb1459bf90fb0c52742d143fb431
DEV <dev@gotabrand.com>
2024-12-27 15:33:28 +0700

    init app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import {Button} from "antd";
 
 export default function Home() {
   return (
-    <h1> hello world nextjs with sonzai</h1>
+      <>
+        <h1> hello world nextjs with sonzai</h1>
+        <Button type={"primary"}>Button from ANTD </Button>
+      </>
+
   );
 }
